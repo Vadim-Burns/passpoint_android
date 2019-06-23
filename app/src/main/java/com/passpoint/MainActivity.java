7d43e82d9c939a4920458bc181128d5991b8ea88
len <vadimdendex@gmail.com>
2019-06-23 18:08:02 +0300

    main activity is ready

--- app/src/main/java/com/passpoint/MainActivity.java
+++ app/src/main/java/com/passpoint/MainActivity.java
@@ -25,7 +25,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.passpoint.passpoint.R;
@@ -61,57 +64,57 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
-            Log.w(TAG, "Not granted");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET}, 1);
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            finish();
-        }
-
-        Log.w(TAG, "Access granted");
-
-        //getting path to our document
-        path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" + FileName;
-        Log.w(TAG, "Absolute path to file: " + path);
-
-        try {
-
-            File pdffile = new File(path);
-            pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(pdffile, ParcelFileDescriptor.MODE_READ_ONLY));
-
-        } catch (FileNotFoundException e) {
-            Log.w(TAG, "File doesn't exists");
-
-            if (isOnline()) Toast.makeText(this, "Документ отсутствует.\nПожалуйста подождите. Когда он будет скачен, перезапустите приложение", Toast.LENGTH_LONG).show();
-            else Toast.makeText(this, "Документ отсутствует.\nБез доступа к сети скачать документ невозможно", Toast.LENGTH_LONG).show();
-
-            getDoc();
-
-            try {
-                //new try to get document
-                File pdffile = new File(path);
-                pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(pdffile, ParcelFileDescriptor.MODE_READ_ONLY));
-            } catch (FileNotFoundException error) {
-                Log.w(TAG, "File not found the second time");
-            } catch (IOException error) {
-                Log.w(TAG, e.getMessage());
-            }
-        } catch (IOException e) {
-            Log.w(TAG, e.getMessage());
-        }
-
-        try {
-            Bitmap bitmap = Bitmap.createBitmap(this.getResources().getDisplayMetrics().widthPixels + 10, this.getResources().getDisplayMetrics().heightPixels + 10, Bitmap.Config.ARGB_8888);
-
-            pdfRenderer.openPage(0).render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
-
-            Log.w(TAG, "setting bitmap");
-            ImageView imageView = findViewById(R.id.pdf_view);
-            imageView.setImageBitmap(bitmap);
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
-        }
+//        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+//            Log.w(TAG, "Not granted");
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET}, 1);
+//        }
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//            finish();
+//        }
+//
+//        Log.w(TAG, "Access granted");
+//
+//        //getting path to our document
+//        path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" + FileName;
+//        Log.w(TAG, "Absolute path to file: " + path);
+//
+//        try {
+//
+//            File pdffile = new File(path);
+//            pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(pdffile, ParcelFileDescriptor.MODE_READ_ONLY));
+//
+//        } catch (FileNotFoundException e) {
+//            Log.w(TAG, "File doesn't exists");
+//
+//            if (isOnline()) Toast.makeText(this, "Документ отсутствует.\nПожалуйста подождите. Когда он будет скачен, перезапустите приложение", Toast.LENGTH_LONG).show();
+//            else Toast.makeText(this, "Документ отсутствует.\nБез доступа к сети скачать документ невозможно", Toast.LENGTH_LONG).show();
+//
+//            getDoc();
+//
+//            try {
+//                //new try to get document
+//                File pdffile = new File(path);
+//                pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(pdffile, ParcelFileDescriptor.MODE_READ_ONLY));
+//            } catch (FileNotFoundException error) {
+//                Log.w(TAG, "File not found the second time");
+//            } catch (IOException error) {
+//                Log.w(TAG, e.getMessage());
+//            }
+//        } catch (IOException e) {
+//            Log.w(TAG, e.getMessage());
+//        }
+//
+//        try {
+//            Bitmap bitmap = Bitmap.createBitmap(this.getResources().getDisplayMetrics().widthPixels + 10, this.getResources().getDisplayMetrics().heightPixels + 10, Bitmap.Config.ARGB_8888);
+//
+//            pdfRenderer.openPage(0).render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+//
+//            Log.w(TAG, "setting bitmap");
+//            ImageView imageView = findViewById(R.id.pdf_view);
+//            imageView.setImageBitmap(bitmap);
+//        } catch (Exception e) {
+//            Log.e(TAG, e.getMessage());
+//        }
     }
 
 
@@ -128,30 +131,24 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.mybutton) {
             if (lang == "RU") {
+                TextView textView = (TextView) findViewById(R.id.rules_view);
+                textView.setText(getResources().getString(R.string.rules_eng));
+                Button button = (Button) findViewById(R.id.sign_activity_button);
+                button.setText(getResources().getString(R.string.next_button_eng));
                 item.setIcon(R.drawable.eng_button);
                 lang = "ENG";
             } else {
+                TextView textView = (TextView) findViewById(R.id.rules_view);
+                textView.setText(getResources().getString(R.string.rules_rus));
+                Button button = (Button) findViewById(R.id.sign_activity_button);
+                button.setText(getResources().getString(R.string.next_button_rus));
                 item.setIcon(R.drawable.ru_button);
                 lang = "RU";
             }
+            ScrollView scrollView = (ScrollView) findViewById(R.id.scrollview);
+            scrollView.scrollTo(0, 0);
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void getDoc() {
-        DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(url);
-        DownloadManager.Request request = new DownloadManager.Request(uri);
-
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, FileName);
-
-        Log.w(TAG, "Starting request");
-
-        dm.enqueue(request);
-
-        finish();
     }
 
     public void sign(View view) {
